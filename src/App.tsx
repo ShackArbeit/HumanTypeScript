36c4079d957e39c7f160e28042ce8a8d0ad3d87d
Shack <g0972222165@gmail.com>
2023-10-01 19:04:46 +0800

    Api Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {useState,useEffect} from 'react'
+
+
+type UserData={
+  id:number
+  job:string
+  salary:number
+}
 
 function App() {
-
+  const[data,setData]=useState<UserData[]>([])
+  useEffect(()=>{
+    const fetchApi=async()=>{
+      try{
+        const response=await fetch('http://localhost:8000/user')
+        if(!response){
+          throw new Error('Can not get data from port 8000 !')
+        }
+        const Userdata=await response.json()
+        console.log(Userdata)
+        setData(Userdata)
+      }catch(error){
+        console.log()
+      }
+    }
+    fetchApi()
+   },[])
+  
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
       <h1>HumanDeisgn TypeScript</h1>
+      {data.map((item) => (
+            <div key={item.id}>
+             <p>Job: {item.job}</p>
+             <p>Salary: {item.salary}</p>
+              </div>
+          ))}
     </>
   )
 }

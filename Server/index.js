9c94efcc8fd25faaefdb59aa7fe357e9a917686a
Shack Lin <g0972222165@gmail.com>
2023-11-14 17:38:55 +0800

    Add Some Booking Setting

--- Server/index.js
+++ Server/index.js
@@ -11,11 +11,118 @@ const {authorHumanDesign}=require('./WhatIsHumanDesign')
 const {energyHumanDesign}=require('./WhatIsHumanDesign')
 const {roadHumanDesign}=require('./WhatIsHumanDesign')
 const { writerHumanDesign} =require('./WhatIsHumanDesign')
+const {isDesktopContent}=require('./BookingIntroduction')
+const {isMobile}=require('./BookingIntroduction')
 const cors=require('cors')
 const app = express();
 
 
 app.use(cors())
+
+const mongo = require('mongodb')
+const url = "mongodb+srv://wang8119:wang8119@cluster0.w3kipgk.mongodb.net/?retryWrites=true&w=majority"
+const client = new mongo.MongoClient(url);
+let db = null
+async function initDB() {
+      try {
+            await client.connect()
+            console.log('連線成功')
+            db = client.db("myWebsite");
+  
+      } catch (err) {
+            console.log('連線失敗', err)
+            return
+      }
+}
+initDB()
+
+const dayjs = require('dayjs');
+
+app.post('/saveDateTime', async (req, res) => {
+  try {
+
+    const collection = db.collection('ForBooking');
+    const { selectDateTime } = req.body;
+    const newBooking = new Date(selectDateTime);
+   // 將存放的時間點做前後 90 分鐘的區間設定
+    const startTime = dayjs(newBooking).subtract(90, 'minutes');
+    const endTime = dayjs(newBooking).add(90,'minutes');
+  // 先向集合內搜尋存在區間內的所有可能
+    const existingReservations = await collection.find({
+      $or: [
+        {
+          $and: [
+            { Year: { $eq: newBooking.getFullYear() } },
+            { Month: { $eq: newBooking.getMonth() } },
+            { Day: { $eq: newBooking.getDate() } },
+            { $or: [
+                { $and: [
+                    { Hour: { $eq: startTime.hour() } },
+                    { Minute: { $gte: startTime.minute() } },
+                ] },
+                { $and: [
+                    { Hour: { $eq: endTime.hour() } },
+                    { Minute: { $lte: endTime.minute() } },
+                ] },
+                { $and: [
+                    { Hour: { $gt: startTime.hour() } },
+                    { Hour: { $lt: endTime.hour() } },
+                ] },
+            ] },
+          ],
+        },
+      ],
+    }).toArray();
+    // 若有搜尋到，則將所有符合條件且已存放在 MongoDB 的資料透過解構賦值返回給前端
+    if (existingReservations.length > 0) {
+      for(i=0;i<existingReservations.length;i++){
+        const { Year, Month, Day, Hour, Minute } = existingReservations[i];
+        return res.status(400).json({
+          success: false,
+          message: 'Reservation time is already booked. Please choose another time.',
+          Year,
+          Month,
+          Day,
+          Hour,
+          Minute
+        });
+      }
+    }
+    // 若沒有搜尋到，就額外新增一筆預約資料
+    else{
+      const year = newBooking.getFullYear();
+      const month = newBooking.getMonth();
+      const day = newBooking.getDate();
+      const hour = newBooking.getHours();
+      const minute = newBooking.getMinutes();
+  
+      await collection.insertOne({
+        Year: year,
+        Month: month,
+        Day: day,
+        Hour: hour,
+        Minute: minute,
+      });
+      res.json({
+        success: true,
+        message: 'DateTime inserted successfully!',
+        Year: year,
+        Month: month,
+        Day: day,
+        Hour: hour,
+        Minute: minute,
+      });
+    } 
+  } catch (error) {
+    console.error('Error inserting DateTime into MongoDB:', error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+
+
+
+
 
 
 app.get("/", (req , res) => {
@@ -74,6 +181,17 @@ app.get('/human/writerHuman',(req,res)=>{
 app.get('/human/roadHuman',(req,res)=>{
   res.send(roadHumanDesign)
 })
+// 以下為預約系統的部分
+app.get('/bookingIntroduction/isDesktop',(req,res)=>{
+  res.send(isDesktopContent)
+})
+app.get('/bookingIntroduction/isMobile',(req,res)=>{
+  res.send(isMobile)
+})
+
+
+
+
 app.listen(9000,()=>{
       console.log('Server running at port 9000 !')
 })
---
+++ src/PageComponents/CalendarComponents/BasicCalendar.tsx
@@ -0,0 +1,152 @@
+import style from '../../CssModules/Calendar.module.css'
+// import {Link} from 'react-router-dom'
+import {  useMediaQuery } from '@mui/material';
+import {useState} from 'react'
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn'; 
+import { DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+dayjs.locale('zh-cn');
+
+
+
+
+export default function BasicCalendar() {
+const history = useNavigate ();
+  const isDesktop = useMediaQuery('(min-width:576px)');
+  const isMobile=useMediaQuery('(max-width:576px');
+  const[selectDateTime,setSelectDateTime]=useState< dayjs.Dayjs| null>(null)
+  const handleSelectDateTime = (newDateTime:any) => {
+    setSelectDateTime(newDateTime);
+  };
+  const handleSendDateTime = async () => {
+    try {
+      const currentDate = dayjs();
+      dayjs.locale('zh-cn');
+      const selectedDate = dayjs(selectDateTime);
+      const formattedDate = selectedDate.format('YYYY/MM/DD A hh:mm');
+  
+      // 防止選取過去的日期
+      if (selectedDate.isBefore(currentDate, 'day')) {
+        alert('你所選取的日期已經過去，請重新選擇');
+        history('/booking');
+        return;
+      }
+  
+      // 只接受三天前的預約
+      const minimumReservationDate = currentDate.add(3, 'day');
+      if (selectedDate.isBefore(minimumReservationDate, 'day')) {
+        alert(`抱歉，無法接受當日預約，只接受${minimumReservationDate.format('YYYY年MM月DD日')}起的預約`);
+        return;
+      // 若都不是前面用前端判斷的例外情形就進入後端 Api 判斷的邏輯
+      } else {
+        // 先向後端 Api 發送 Post 請求，將資料放入資料庫內
+        const response = await fetch('http://localhost:8000/saveDateTime', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ selectDateTime }),
+        });
+        // 將資料 JSON 格式化
+        const responseData = await response.json();
+        // 若是成功放入資料庫則返回放入成功的訊息並 Alert 出來
+        if (responseData.success) {
+          alert(`我們已成功接受您於 ${formattedDate} 的預約了 !`);
+          console.log(`我們已成功接受您於 ${formattedDate} 的預約了!`);
+        // 這裡透過後端的 Api 先比較所要放入的資料時間點是否存在已經在 MongoDB 資料庫內所存放的時間點之前後
+        // 90 分鐘的區間內
+        } else {
+        // responseData 是 status 為 400 時所返回的物件，並透過解構賦值存放在變數 Year,Month,Day,Hour,Minute 中
+          const { Year, Month, Day, Hour, Minute } = responseData;
+        // 設定前端要 Alert 訊息的變數，將前後 90 分鐘的區間透過 day.js 做計算
+          const bookingTime = dayjs(`${Year}-${Month + 1}-${Day} ${Hour}:${Minute}`);
+          const latestBookingTime = bookingTime.subtract(90, 'minutes');
+          const earlyBookingTime = bookingTime.add(90, 'minutes');
+          alert(`此時間屬在其他預約的時段內，你最早可於 ${Year} / ${Month + 1} /  ${Day} / ${latestBookingTime.format('HH:mm')} 前
+          或 ${Year} /${Month + 1} / ${Day} / ${earlyBookingTime.format('HH:mm')}  後預約`);
+        }
+      }
+    } catch (error) {
+      console.log('Error send to MongoDB', error);
+    }
+  };
+  
+  
+  
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <div className={style.BasicCalendarContainer}>
+    <h3 style={{marginBottom:"3rem"}}>請點選日期及時間</h3>
+      {isDesktop?(
+        <>
+        <DemoItem  >
+        <DesktopDateTimePicker 
+        defaultValue={dayjs('2023-11-06T18:30')}
+        sx={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          width:"350px",
+          border:'1px solid rgba(0,0,0,0.8)',
+          transform: "translate(-50%, -50%)",
+          '&:hover': {
+            border:'1px solid #2fffe1;'
+          }}} 
+          format="YYYY年MM月DD日 hh:mm A "
+          
+          slotProps={{ textField: { variant: 'outlined' } }}
+          value={selectDateTime}
+          onChange={handleSelectDateTime}
+          />
+      </DemoItem>
+      {selectDateTime && (
+        
+          <Button 
+            onClick={handleSendDateTime}
+            variant="contained" 
+            size='large'
+            sx={{
+              position:'relative',
+              top:'4rem',
+              fontWeight:900,
+              fontSize:'20px',
+              '&:hover':{
+                border:'2px solid #ffa811',
+                color:'black',
+                backgroundColor:'#fff'
+              }
+            }}>
+            確定送出
+          </Button>
+      
+      )}
+  </>
+) : null}
+
+      {isMobile?( <DemoItem >
+        <MobileDateTimePicker 
+        defaultValue={dayjs('2023-11-06T18:30')}
+        sx={{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          width:"350px",
+          border:'1px solid rgba(0,0,0,0.8)',
+          transform: "translate(-50%, -50%)",
+          '&:hover': {
+              border:'1px solid #2fffe1;'
+            },
+        }}
+          format="YYYY年MM月DD日 hh:mm A "
+           />
+      </DemoItem>):null}
+    </div>
+  </LocalizationProvider>
+  );
+}
